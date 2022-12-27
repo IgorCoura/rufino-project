dbeac2ae28d5ea7d38f24cbc29f7bb4ded48405d
Igor de Brito Coura <igor-coura@hotmail.com>
2022-12-26 23:46:09 -0300

    Create MaterialPurchase.API

---
+++ back-end/Services/MaterialPurchase/MaterialPurchase.API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+});
+
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ back-end/Services/MaterialPurchase/MaterialPurchase.Domain/Entities/Material.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialPurchase.Domain.Entities
+{
+    public class Material
+    {
+        private string _name = string.Empty;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            protected set
+            {
+                _name = value.ToUpper();
+            }
+        }
+
+        public string Description { get; set; } = string.Empty;
+        public string Unity { get; set; } = string.Empty;
+    }
+}
