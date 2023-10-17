3e5106cf66e4716bdeaa7be58ec15715c900414f
Igor de Brito Coura <igor-coura@hotmail.com>
2023-10-17 18:07:44 -0300

    Remover old projects

--- back-end/OldProjects/AuthenticateApi/Authenticate.Api/Model/LoginModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Authenticate.Api.Model
-{
-    public record LoginModel(
-        string Username,
-        string Password
-        );
-}
--- back-end/Services/Identity/Security.Jwt/Core/JwkContants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Security.Jwt.Core;
-
-public class JwkContants
-{
-    public static string CurrentJwkCache => $"NETDEVPACK-CURRENT-SECURITY-KEY";
-    public static string JwksCache => $"NETDEVPACK-JWKS";
-}
