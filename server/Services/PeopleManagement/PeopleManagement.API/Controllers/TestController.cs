b1efc9858e1cbc2a13e8930e8a23c54ca07b2e8b
Igor de Brito Coura <igor-coura@hotmail.com>
2025-07-18 17:13:20 -0300

    Change for test v2

--- server/Services/PeopleManagement/PeopleManagement.API/Controllers/TestController.cs
+++ server/Services/PeopleManagement/PeopleManagement.API/Controllers/TestController.cs
@@ -10,7 +10,7 @@ namespace PeopleManagement.API.Controllers
         [HttpGet]
         public IActionResult GetTest()
         {
-            return Ok("It's ok. v1");
+            return Ok("It's ok. v2");
         }
 
         [HttpGet("auth")]
