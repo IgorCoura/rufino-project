780758f1943466ad2982dd877a9d0160479d3509
Igor de Brito Coura <igor-coura@hotmail.com>
2025-08-01 11:43:26 -0300

    resolve document deprecated service

--- server/Services/PeopleManagement/PeopleManagement.Services/Services/DocumentDepreciationService.cs
+++ server/Services/PeopleManagement/PeopleManagement.Services/Services/DocumentDepreciationService.cs
@@ -1,15 +1,24 @@
-﻿using PeopleManagement.Domain.AggregatesModel.DocumentAggregate.Interfaces;
-using PeopleManagement.Domain.AggregatesModel.DocumentAggregate;
+﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using PeopleManagement.Domain.AggregatesModel.DocumentAggregate;
+using PeopleManagement.Domain.AggregatesModel.DocumentAggregate.Interfaces;
+using PeopleManagement.Domain.AggregatesModel.EmployeeAggregate;
+using PeopleManagement.Domain.AggregatesModel.EmployeeAggregate.Interfaces;
+using PeopleManagement.Domain.AggregatesModel.RequireDocumentsAggregate;
+using PeopleManagement.Domain.AggregatesModel.RequireDocumentsAggregate.Interfaces;
+using System.Threading;
 
 
 namespace PeopleManagement.Services.Services
 {
-    public class DocumentDepreciationService(IDocumentRepository documentRepository, ILogger<DocumentDepreciationService> logger) : IDocumentDepreciationService
+    public class DocumentDepreciationService(ILogger<DocumentDepreciationService> logger, IDocumentRepository documentRepository, 
+        IRequireDocumentsRepository requireDocumentsRepository, IEmployeeRepository employeeRepository) : IDocumentDepreciationService
     {
         private readonly IDocumentRepository _documentRepository = documentRepository;
         private readonly ILogger<DocumentDepreciationService> _logger = logger;
+        private readonly IRequireDocumentsRepository _requireDocumentsRepository = requireDocumentsRepository;
+        private readonly IEmployeeRepository _employeeRepository = employeeRepository;
         public async Task DepreciateExpirateDocument(Guid documentUnitId, Guid documentId, Guid companyId,
             CancellationToken cancellationToken = default)
         {
@@ -25,8 +34,21 @@ namespace PeopleManagement.Services.Services
                 return;
             }
 
-            var newDocumentUnitId = Guid.NewGuid();
-            document.MakeAsDocumentExpired(documentUnitId, newDocumentUnitId);
+           
+
+            var isAssociation = await DocumentHasAssociation(document, cancellationToken);
+
+            if (isAssociation)
+            {
+                var newDocumentUnitId = Guid.NewGuid();
+                document.MakeAsDocumentDeprecated(documentUnitId, newDocumentUnitId);
+                document.NewDocumentUnit(Guid.NewGuid());
+            }
+            else
+            {
+                document.MakeAsDeprecated();
+            }
+
 
             await _documentRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
 
@@ -49,13 +71,33 @@ namespace PeopleManagement.Services.Services
                 return;
             }
 
-            var newDocumentUnitId = Guid.NewGuid();
-            document.MakeAsWarning(documentUnitId, newDocumentUnitId);
+            var isAssociation = await DocumentHasAssociation(document, cancellationToken);
+
+            if (isAssociation)
+            {
+                document.MakeAsWarning(documentUnitId);
+            }
+            else
+            {
+                document.MakeAsDeprecated();
+            }
+
 
             await _documentRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
 
             _logger.LogInformation("Document with ID {DocumentId} has been marked as warning for company {CompanyId}.", documentId, companyId);
         }
 
+        public async Task<bool> DocumentHasAssociation(Document document, CancellationToken cancellationToken)
+        {
+            RequireDocuments? reqDocument = await _requireDocumentsRepository.FirstOrDefaultAsync(x => x.Id == document.RequiredDocumentId && x.CompanyId == document.CompanyId, cancellation: cancellationToken);
+            Employee? employee = await _employeeRepository.FirstOrDefaultAsync(x => x.Id == document.EmployeeId && x.CompanyId == document.CompanyId, cancellation: cancellationToken);
+
+            if (employee is not null && reqDocument is not null)
+            {
+                return employee.IsAssociation(reqDocument.AssociationId);
+            }
+            return false;
+        }
     }
 }
