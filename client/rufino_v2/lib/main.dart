2aeaab00ab1845ae3b1cbc78eec9f2cb6f4b7f45
Igor de Brito Coura <igor-coura@hotmail.com>
2026-04-02 11:28:59 -0300

    feat: implement permission caching with SharedPreferences and update related components

--- client/rufino_v2/lib/main.dart
+++ client/rufino_v2/lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
 import 'core/config/app_config.dart';
 import 'core/config/dev_http_overrides_stub.dart'
     if (dart.library.io) 'core/config/dev_http_overrides.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   AppConfig.assertConfigured();
 
   if (AppConfig.isDevelop) {
     applyDevHttpOverrides();
   }
 
-  runApp(const App());
+  final prefs = await SharedPreferences.getInstance();
+
+  runApp(App(prefs: prefs));
 }
--- client/rufino_v2/test/testing/fakes/fake_permission_repository.dart
+++ client/rufino_v2/test/testing/fakes/fake_permission_repository.dart
@@ -4,6 +4,7 @@ import 'package:rufino_v2/domain/repositories/permission_repository.dart';
 
 class FakePermissionRepository implements PermissionRepository {
   List<Permission> _permissions = const [];
+  List<Permission>? _cachedPermissions;
   bool _shouldFail = false;
 
   void setPermissions(List<Permission> permissions) =>
@@ -11,11 +12,29 @@ class FakePermissionRepository implements PermissionRepository {
 
   void setShouldFail(bool value) => _shouldFail = value;
 
+  void setCachedPermissions(List<Permission>? permissions) =>
+      _cachedPermissions = permissions;
+
   @override
   Future<Result<List<Permission>>> fetchPermissions() async {
     if (_shouldFail) {
       return Result.error(Exception('Permission fetch failed'));
     }
     return Result.success(_permissions);
+  }
+
+  @override
+  Future<List<Permission>?> getCachedPermissions() async {
+    return _cachedPermissions;
+  }
+
+  @override
+  Future<void> cachePermissions(List<Permission> permissions) async {
+    _cachedPermissions = permissions;
+  }
+
+  @override
+  Future<void> clearCachedPermissions() async {
+    _cachedPermissions = null;
   }
 }
