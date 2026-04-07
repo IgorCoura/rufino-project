84e9bc82459e2947e2ceb0433bf548640dacadde
Igor de Brito Coura <igor-coura@hotmail.com>
2026-04-07 00:57:32 -0300

    Adiciona geração de ID de requisição e atualiza cabeçalhos nas requisições da API

--- client/rufino_v2/lib/data/services/company_api_service.dart
+++ client/rufino_v2/lib/data/services/company_api_service.dart
@@ -1,8 +1,19 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:http/http.dart' as http;
 
 import '../models/company_api_model.dart';
 import 'http_status_helper.dart';
+
+String _newRequestId() {
+  final rnd = Random.secure();
+  final bytes = List<int>.generate(16, (_) => rnd.nextInt(256));
+  bytes[6] = (bytes[6] & 0x0f) | 0x40; // version 4
+  bytes[8] = (bytes[8] & 0x3f) | 0x80; // variant
+  String hex(int i) => bytes[i].toRadixString(16).padLeft(2, '0');
+  final b = List<String>.generate(16, hex);
+  return '${b[0]}${b[1]}${b[2]}${b[3]}-${b[4]}${b[5]}-${b[6]}${b[7]}-${b[8]}${b[9]}-${b[10]}${b[11]}${b[12]}${b[13]}${b[14]}${b[15]}';
+}
 
 class CompanyApiService {
   CompanyApiService({
@@ -15,10 +26,11 @@ class CompanyApiService {
   final String baseUrl;
   final Future<String> Function() getAuthHeader;
 
-  Future<Map<String, String>> _headers() async {
+  Future<Map<String, String>> _headers({bool withRequestId = false}) async {
     return {
       'Authorization': await getAuthHeader(),
       'Content-Type': 'application/json',
+      if (withRequestId) 'x-requestid': _newRequestId(),
     };
   }
 
@@ -41,7 +53,7 @@ class CompanyApiService {
     final uri = Uri.https(baseUrl, '/api/v1/company');
     final response = await client.post(
       uri,
-      headers: await _headers(),
+      headers: await _headers(withRequestId: true),
       body: jsonEncode(model.toCreateJson()),
     );
     checkHttpStatus(response);
@@ -53,7 +65,7 @@ class CompanyApiService {
     final uri = Uri.https(baseUrl, '/api/v1/company');
     final response = await client.put(
       uri,
-      headers: await _headers(),
+      headers: await _headers(withRequestId: true),
       body: jsonEncode(model.toJson()),
     );
     checkHttpStatus(response);
