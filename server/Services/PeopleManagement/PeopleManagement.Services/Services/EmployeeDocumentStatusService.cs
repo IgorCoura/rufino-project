6bca538dd9c635eb1e1f890523b20b83f936ba6e
Igor de Brito Coura <igor-coura@hotmail.com>
2026-04-24 11:39:23 -0300

    fix EmployeeDocumentStatusService

--- server/Services/PeopleManagement/PeopleManagement.Services/Services/EmployeeDocumentStatusService.cs
+++ server/Services/PeopleManagement/PeopleManagement.Services/Services/EmployeeDocumentStatusService.cs
@@ -9,26 +9,24 @@ namespace PeopleManagement.Services.Services
         public EmployeeDocumentStatus DetermineStatusFromDocumentStatuses(List<DocumentStatus> documentStatuses)
         {
 
-            //return EmployeeDocumentStatus.Okay;
 
             if (documentStatuses == null || documentStatuses.Count == 0)
             {
                 return EmployeeDocumentStatus.Okay;
             }
 
-            // Prioridade 1: Warning
+            if (documentStatuses.Any(x => x == DocumentStatus.RequiresDocument || x == DocumentStatus.RequiresValidation))
+            {
+                return EmployeeDocumentStatus.RequiresAttention;
+            }
+
             if (documentStatuses.Any(x => x == DocumentStatus.Warning))
             {
                 return EmployeeDocumentStatus.Warning;
             }
 
-            // Prioridade 2: Qualquer status que não seja OK ou AwaitingSignature
-            if (documentStatuses.Any(x => x != DocumentStatus.OK && x != DocumentStatus.AwaitingSignature))
-            {
-                return EmployeeDocumentStatus.RequiresAttention;
-            }
-
             return EmployeeDocumentStatus.Okay;
         }
+
     }
 }
